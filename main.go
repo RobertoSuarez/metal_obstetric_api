f1c79744dbca4bb5853c00c26bdbdc553880d868
RobertoSuarez <electrosonix12@gmail.com>
2022-12-21 01:27:51 -0500

    server con fiber montado

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
-	fmt.Println("Hola, mundo")
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		user := User{
+			ID:       1,
+			Nombre:   "Roberto",
+			Apellido: "Suárez",
+		}
+		return c.JSON(user)
+	})
+
+	app.Listen(":3000")
+}
+
+type User struct {
+	ID       int    `json:"id"`
+	Nombre   string `json:"nombre"`
+	Apellido string `json:"apellido"`
 }
